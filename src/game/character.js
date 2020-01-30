9ebb4070a1709525236b7b430f9804daed7db2da
Richard Lu <sungsta@berkeley.edu>
2020-01-29 18:16:00 -0800

    kind of fix flickering problem

--- src/game/character.js
+++ src/game/character.js
@@ -77,10 +77,11 @@ export default class Character {
     var img = new Image();
     var pos = this.position;
     var lastDirLR = this.lastDirLR;
-    img.onload = function() {
-      drawImage(ctx, img, pos[0], pos[1], 84, 75, 0, (lastDirLR === 'left' ? true : false), false);
-    }
+    // img.onload = function() {
+    //   drawImage(ctx, img, pos[0], pos[1], 84, 75, 0, (lastDirLR === 'left' ? true : false), false);
+    // }
     img.src = this.animations[this.state].frameData[this.lastDirUD][this.frame];
+    drawImage(ctx, img, pos[0], pos[1], 84, 75, 0, (lastDirLR === 'left' ? true : false), false);
   }
 
   move(dir) {
