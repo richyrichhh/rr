4a4bca30942e5d9d0009578d78b3547760453ebd
Richard Lu <sungsta@berkeley.edu>
2020-01-31 18:09:26 -0800

    add menu logic

--- src/index.js
+++ src/index.js
@@ -14,13 +14,19 @@ window.Enemy = Enemy;
 window.Character = Character;
 
 let game = new Game(canvas);
-game.start();
+game.startMenu();
 const moveLeft = _.throttle(() => game.player.move('left'), 164);
 const moveDown = _.throttle(() => game.player.move('down'), 164);
 const moveRight = _.throttle(() => game.player.move('right'), 164);
 const moveUp = _.throttle(() => game.player.move('up'), 164);
 const attack = _.throttle(() => game.player.attack(), 1000);
 // let view = new GameView(game, canvas.getContext("2d"));
+document.addEventListener('keydown', gameStart);
+let gameStart = () => {
+  game.start();
+  game.menu = null;
+  document.removeEventListener('keydown', gameStart);
+}
 document.addEventListener('keydown', e => {
   if (game.player.state === 'death') return null;
   if(e.code === 'ArrowLeft') {
--- src/menu/start-menu.js
+++ src/menu/start-menu.js
@@ -7,7 +7,7 @@ export default class startMenu {
   render() {
     let img = new Image();
     img.src = '../src/game/animations/logo.png';
-    this.ctx.drawImage(img, this.game.canvas.dimensions.width / 2, this.game.canvas.dimensions.height / 2 + 100);
-
+    this.ctx.drawImage(img, this.game.canvas.width / 2, this.game.canvas.height / 2 + 100);
+    this.ctx.fillText(`The referees have all gone blind and are on a rampage! Now it's up to Charles Barkley to help them see again.`, this.game.canvas.width / 2, this.game.canvas.height / 2 - 100, 500);
   }
 }
