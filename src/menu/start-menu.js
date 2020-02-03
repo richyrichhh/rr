8bfc11065f3a7d6405e1a2117af913d8a3576104
Richard Lu <sungsta@berkeley.edu>
2020-02-02 19:02:42 -0800

    rewrite start menu story

--- src/menu/start-menu.js
+++ src/menu/start-menu.js
@@ -19,8 +19,8 @@ export default class startMenu {
     bg.onload = () =>  {
       this.ctx.drawImage(bg, 0, 0);
       this.ctx.drawImage(img, this.game.canvas.width / 2 - 100, this.game.canvas.height / 2 - 200);
-      this.ctx.fillText(`The referees have all gone blind and are on a rampage!`, this.game.canvas.width / 2 - 240, this.game.canvas.height / 2 + 50, 500);
-      this.ctx.fillText(`Now it's up to Charles Barkley to help them see again.`, this.game.canvas.width / 2 - 240, this.game.canvas.height / 2 + 80, 500);
+      this.ctx.fillText(`The referees are blind!`, this.game.canvas.width / 2 - 240, this.game.canvas.height / 2 + 50, 500);
+      this.ctx.fillText(`It's up to Charles Barkley to help them see the light.`, this.game.canvas.width / 2 - 240, this.game.canvas.height / 2 + 80, 500);
       this.ctx.fillText(`Controls:`, this.game.canvas.width / 2 - 300, this.game.canvas.height / 2 + 130, 500);
       this.ctx.fillText(`↑↓←→ Movement`, this.game.canvas.width / 2 - 300, this.game.canvas.height / 2 + 155, 500);
       this.ctx.fillText(`A    Attack`, this.game.canvas.width / 2 - 300, this.game.canvas.height / 2 + 180, 500);
