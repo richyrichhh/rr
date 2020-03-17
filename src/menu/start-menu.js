1f10a10de1935f4b35be0530288e97a6a3844e1e
Richard Lu <sungsta@berkeley.edu>
2020-03-16 18:48:14 -0700

    remove religious stuff, changed rampage to rumble for more pg13

--- src/menu/start-menu.js
+++ src/menu/start-menu.js
@@ -14,11 +14,11 @@ export default class startMenu {
     // this.ctx.clearRect(0, 0, this.game.canvas.width, this.game.canvas.height);
     this.ctx.fillStyle = "black";
     let img = new Image();
-    img.src = '../src/game/animations/logo.png';
+    img.src = '../src/game/animations/cb-logo.png';
     let bg = new Image();
     bg.onload = () =>  {
       this.ctx.drawImage(bg, 0, 0);
-      this.ctx.drawImage(img, this.game.canvas.width / 2 - 100, this.game.canvas.height / 2 - 200);
+      this.ctx.drawImage(img, Math.floor((this.game.canvas.width / 2) - (485 / 2)), 50, 485, 237);
       this.ctx.fillText(`The referees are blind!`, this.game.canvas.width / 2 - 240, this.game.canvas.height / 2 + 50, 500);
       this.ctx.fillText(`It's up to Charles Barkley to help them see the light.`, this.game.canvas.width / 2 - 240, this.game.canvas.height / 2 + 80, 500);
       this.ctx.fillText(`Controls:`, this.game.canvas.width / 2 - 300, this.game.canvas.height / 2 + 130, 500);
